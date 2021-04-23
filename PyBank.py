07a1e130a6cd081e1329ed8479860d3d7b461275
Andy Jagdeo <jagdeoan@kean.edu>
2021-04-23 17:41:33 -0400

    Update PyBank.py

--- PyBank.py
+++ PyBank.py
@@ -1,1 +1,13 @@
 print ('pybank_challenge')
+# Create file paths across operating systems
+import os
+# module for reading csv files
+import csv
+# pass individual sub components 
+csvpath = os.path.join('Resources', 'budget_data.csv')
+#../resources/budget_data.csv
+
+#open path and store file as variable "csvfile"
+with open (csvpath) as csvfile:
+#call csv reader
+csvreader = csv.reader(csvfile, delimiter=',')
